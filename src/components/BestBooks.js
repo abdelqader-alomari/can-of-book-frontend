a1348243d156e830494211e404f1bbd743caeb33
abdelqader-alomari <aboud.coding@gmail.com>
2021-08-11 05:43:43 +0300

    lab-13 frontend

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
-import { Carousel } from 'react-bootstrap'
-require('dotenv').config();
+import { Carousel, Button } from 'react-bootstrap'
+import FormModal from './FormModal';
 
 export class BestBooks extends React.Component {
 
@@ -10,7 +10,8 @@ export class BestBooks extends React.Component {
         super(props)
         this.state = {
             numberOfBooks: 0,
-            BooksData: '',
+            BooksData: [],
+            displayAddModal: false
         }
     }
     componentDidMount = () => {
@@ -24,12 +25,60 @@ export class BestBooks extends React.Component {
             })
         })
     }
+    handleDisplayModal = () => {
+        this.setState({ displayAddModal: !this.state.displayAddModal });
+    }
+
+    handleAddBookForm = (e) => {
+
+        e.preventDefault();
+        this.handleDisplayModal(); // hide the modal after form submission
+        // console.log('name: ', e.target.catName.value);
+        // console.log('breed: ', e.target.catBreed.value);
+        // console.log('Image: ', e.target.catImage.value);
+
+        const body = {
+            email: this.props.auth0.user.email, // we are getting the email of the user from auth0
+            title: e.target.title.value,
+            description: e.target.description.value,
+            status: e.target.status.value,
+            img_url: e.target.img_url.value,
+        };
+
+        axios.post(`${process.env.REACT_APP_PORT}/book`, body).then(booksData => {
+
+            this.state.booksData.push(booksData.data);
+            this.setState({
+                booksData: this.state.booksData
+            });
+        }).catch(error => alert(error));
+    }
+    handleDeleteBook = (bookId) => {
+        axios.delete(`${process.env.REACT_APP_PORT}/book/${bookId}`).then(res => {
+
+            if (res.data.ok === 1) {
+                const tempBookObj = this.state.booksData.filter(book => book._id !== bookId);
+                this.setState({
+                    booksData: tempBookObj
+                });
+            }
+        }).catch(error => alert(error))
+    }
+
 
     render() {
         return (
             <div>
                 <>
+                    <Button variant="secondary" onClick={() => this.handleDisplayModal()}>Add a book</Button>
+                    <FormModal
+                        show={this.state.displayAddModal}
+                        handleDisplayModal={this.handleDisplayModal}
+                        handleSubmitForm={this.handleAddBookForm}
+                    />
+
                     < Carousel >
+
                         {this.state.numberOfBooks > 0 &&
                             this.state.BooksData.map(value =>
 
@@ -43,10 +92,12 @@ export class BestBooks extends React.Component {
                                     <Carousel.Caption  >
                                         <h3 style={{ fontSize: '18px', backgroundColor: "#333", width: "34%", textAlign: 'center', marginLeft: "34%" }}>{value.title}</h3>
                                         <p style={{ fontSize: '12px', backgroundColor: "#333", width: "34%", textAlign: 'center', marginLeft: "34%" }}>{value.description}</p>
+                                        <p style={{ fontSize: '12px', backgroundColor: "#333", width: "20%", textAlign: 'center', marginLeft: "34%" }}>{value.status}</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
                             )}
-                    </Carousel >                        </>
+                    </Carousel >
+                </>
             </div>
         )
     }
---
+++ src/components/FormModal.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Modal, Button, Form } from 'react-bootstrap';
+
+class FormModal extends React.Component {
+    render() {
+        return (
+            <div>
+                <Modal show={this.props.show} onHide={this.props.handleDisplayModal}>
+                    <Modal.Header>
+                        <Modal.Title>Add Book Form</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={(e) => this.props.handleSubmitForm(e)}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Book Title</Form.Label>
+                                <Form.Control name="title" type="text" placeholder="Enter the book title" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Book Description</Form.Label>
+                                <Form.Control name="description" type="text" placeholder="Enter the book description" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Book Status</Form.Label>
+                                <Form.Control name="status" type="text" placeholder="Enter the book status" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Book Image</Form.Label>
+                                <Form.Control name="img_url" type="text" placeholder="Enter the image URL" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Add Book
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.props.handleDisplayModal}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default FormModal
